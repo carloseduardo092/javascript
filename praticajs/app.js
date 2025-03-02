95cb2b881cb9a32690f90ba9599b10f7009c8d57
kadudev <kadujunior567@gmail.com>
2025-03-02 03:23:43 -0400

    pratica js

---
+++ praticajs/app.js
@@ -0,0 +1,26 @@
+var nome = window.prompt("Qual é o seu nome ?");
+window.alert("Seja bem vindo, " + nome + "!");
+
+let carrinho = [];
+function adicionarProdut(elemento) {
+  let id = elemento.getAttribute("data-id");
+  let nome = elemento.getAttribute("data-nome");
+  let preco = parseFloat(elemento.getAttribute("data-preco"));
+
+  carrinho.push({ id, nome, preco, quantidade: 1 });
+  console.log(carrinho);
+}
+
+function removerProduto(id) {
+  let itemEncontrado = carrinho.find((produto) => produto.id === id);
+  if (itemEncontrado) {
+    if (itemEncontrado.quantidade > 1) {
+      itemEncontrado.quantidade--;
+    } else {
+      let resultado = carrinho.findIndex((produto) => produto.id === id);
+      if (resultado !== -1) {
+        carrinho.splice(resultado, 1);
+      }
+    }
+  }
+}
